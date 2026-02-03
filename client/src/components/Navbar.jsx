30bf6c339d267a600cd2e6b8d3b2cb55f212c175
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-04 00:54:37 +0530

    add navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link><img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/> 
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
