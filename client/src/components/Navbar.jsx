e7055a513e5a721a606dbbb488b7e243a037cf88
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-04 23:34:06 +0530

    add navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,10 +3,30 @@ import {assets} from '../assets/assets'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [user, setUser] = useState(null);
   return (
     <div>
-      <Link><img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/> 
+      <Link to='/'>
+      <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40'/> 
       </Link>
+
+      <div>
+        {
+        user ? 
+        <div></div>
+        :
+        <div>
+          <p>Pricing</p>
+          <button>Login</button>
+        </div>
+        }
+        
+        
+      </div>
+
+
+
     </div>
   )
 }
