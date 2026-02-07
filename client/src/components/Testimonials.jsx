e8182e2267d21a8231acba9c01ca770da9e4333f
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-07 20:07:44 +0530

    add all the changes in  testimonials.jsx

--- client/src/components/Testimonials.jsx
+++ client/src/components/Testimonials.jsx
@@ -5,28 +5,44 @@ const Testimonials = () => {
   return (
     <div className='flex flex-col items-center justify-center my-20 py-12'>
       <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>
-        Customer testimonials
+        Customer Testimonials
       </h1>
+
       <p className='text-gray-500 mb-12'>What Our Users Are Saying</p>
 
-      <div className='flex flex-wrap gap-6'>
-        {testimonialsData.map((testmonial, index) => (
-          <div key={index}>
-            <div>
+      <div className='flex flex-wrap gap-6 justify-center'>
+        {testimonialsData.map((testimonial, index) => (
+          <div
+            key={index}
+            className='bg-white/20 p-12 rounded-lg shadow-md border w-80 m-auto cursor-pointer hover:scale-[1.05] hover:shadow-xl transition-all duration-300'
+          >
+            <div className='flex flex-col items-center'>
               <img
-                src={testmonial.image}
-                alt=''
+                src={testimonial.image}
+                alt={testimonial.name}
                 className='rounded-full w-14'
               />
-              <h2>{testmonial.name}</h2>
-              <p>{testmonial.role}</p>
+
+              <h2 className='text-xl font-semibold mt-3'>{testimonial.name}</h2>
+
+              <p className='text-gray-500 mb-4'>{testimonial.role}</p>
+
               <div className='flex mb-4'>
-                {Array(testmonial.stars).fill().map((item,index)=>
-                (
-                    <img key={index} src={assets.rating_star} alt="" />
-                ))}
+                {Array(testimonial.stars)
+                  .fill()
+                  .map((_, starIndex) => (
+                    <img
+                      key={starIndex}
+                      src={assets.rating_star}
+                      alt='star'
+                      className='w-4 h-4'
+                    />
+                  ))}
               </div>
-              <p>{testmonial.text}</p>
+
+              <p className='text-center text-sm text-gray-600'>
+                {testimonial.text}
+              </p>
             </div>
           </div>
         ))}
