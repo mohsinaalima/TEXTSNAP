b17f89135c40ffbd02a5b9b56d03789c36743810
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-07 12:21:43 +0530

    add changes in  description.jsx

--- client/src/components/Description.jsx
+++ client/src/components/Description.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { assets } from "../assets/assets";
 
 const Description = () => {
   return (
@@ -7,6 +8,31 @@ const Description = () => {
         Create AI Images
       </h1>
       <p className='text-gray-500 mb-8'>Turn your imagination into visuals</p>
+
+      <div className='flex flex-col gap-5 md:gap-14 md:flex-row items-center'>
+        <img
+          src={assets.sample_img_1}
+          alt=''
+          className='w-80 xl:w-96 rounded-lg'
+        />
+        <div>
+          <h2 className='text-3xl font-medium max-w-lg mb-4'>
+            Introducing the AI-Powered Text to Image Generator
+          </h2>
+          <p className='text-gray-600 mb-4'>
+            Easily turn your ideas into reality with our AI image generator.
+            Whether you need beautiful visuals or creative images, our tool
+            converts your text into eye-catching pictures in just a few clicks.
+            Simply imagine it, describe it, and see it come to life instantly.
+          </p>
+          <p className='text-gray-600'>
+            Simply enter a text prompt, and our advanced AI will generate
+            high-quality images in seconds. From product visuals to character
+            designs and portraits, everything is created effortlessly using
+            powerful AI technology. The creative possibilities are endless.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
