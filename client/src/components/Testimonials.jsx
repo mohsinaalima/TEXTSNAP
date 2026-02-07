5f9740c88d97d1b3bd2ceb04e787e208b7b25f7b
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-07 19:38:32 +0530

    add testimonials.jsx

---
+++ client/src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div className='flex flex-col items-center justify-center my-20 py-12'>
+      <h1 className='text-3xl sm:text-4xl font-semibold mb-2'>
+        Customer testimonials
+      </h1>
+      <p className='text-gray-500 mb-12'>What Our Users Are Saying</p>
+    </div>
+  );
+};
+
+export default Testimonials;
