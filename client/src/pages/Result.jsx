d74507a7fb8ff3dd62071d2475c1c6cb73df9625
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-10 23:06:56 +0530

    done result page

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -3,21 +3,27 @@ import { assets } from "../assets/assets";
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
 
-  const [isImageLoaded, setIsImageLoaded] = useState(true);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState('')
 
   return (
     <form className='flex flex-col min-h-[90vh] justify-center items-center'>
       <div>
         <div className='relative'>
           <img src={image} alt='' className='max-w-sm rounded' />
-          <span className='absolute bottom-0 left-0 h-1 bg-blue-500 w-full transition-all duration-[10s]' />
+          <span
+            className={`absolute bottom-0 left-0 h-1 bg-blue-500 ${
+              loading ? "w-full transition-all duration-[10s]" : "w-0"
+            }`}
+          />
         </div>
         <p className={!loading ? "hidden" : ""}>Loading.....</p>
       </div>
       {!isImageLoaded && (
         <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
           <input
+          onChange={e => setInput(e.target.value)} value=
             type='text'
             placeholder='Describe what you want to generate'
             className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'
