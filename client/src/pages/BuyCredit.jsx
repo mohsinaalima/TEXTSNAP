48a5cdcb92f2c32da78c02213625923734668a07
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-10 23:26:04 +0530

    add buycredit

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const BuyCredit = () => {
-  return <div>Buy Credit</div>;
+  return (
+    <div className='min-h-[80vh] text-center pt-14 mb-10'>
+      <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>
+        Our Plans
+      </button>
+      <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'>
+        Choose the plan
+      </h1>
+    </div>
+  );
 };
 
 export default BuyCredit;
--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+
 const Result = () => {
   const [image, setImage] = useState(assets.sample_img_1);
-
   const [isImageLoaded, setIsImageLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    if (!input.trim()) return;
+
+    setLoading(true);
+    setIsImageLoaded(false);
+
+    setTimeout(() => {
+      setImage(assets.sample_img_1);
+      setLoading(false);
+      setIsImageLoaded(true);
+    }, 3000);
+  };
 
   return (
-    <form className='flex flex-col min-h-[90vh] justify-center items-center'>
+    <form
+      onSubmit={onSubmitHandler}
+      className='flex flex-col min-h-[90vh] justify-center items-center'
+    >
       <div>
         <div className='relative'>
           <img src={image} alt='' className='max-w-sm rounded' />
@@ -20,13 +38,15 @@ const Result = () => {
         </div>
         <p className={!loading ? "hidden" : ""}>Loading.....</p>
       </div>
+
       {!isImageLoaded && (
         <div className='flex w-full max-w-xl bg-neutral-500 text-white text-sm p-0.5 mt-10 rounded-full'>
           <input
-          onChange={e => setInput(e.target.value)} value=
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
             type='text'
             placeholder='Describe what you want to generate'
-            className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20 placeholder-color'
+            className='flex-1 bg-transparent outline-none ml-8 max-sm:w-20'
           />
           <button
             type='submit'
@@ -36,12 +56,11 @@ const Result = () => {
           </button>
         </div>
       )}
+
       {isImageLoaded && (
-        <div className='flex gap-2 flex-wrap justify-center text-white text-sm p-0.5 mt-10 rounded-full'>
+        <div className='flex gap-2 flex-wrap justify-center text-sm p-0.5 mt-10'>
           <p
-            onClick={() => {
-              setIsImageLoaded(false);
-            }}
+            onClick={() => setIsImageLoaded(false)}
             className='bg-transparent border border-zinc-900 text-black px-8 py-3 rounded-full cursor-pointer'
           >
             Generate Another
@@ -49,7 +68,7 @@ const Result = () => {
           <a
             href={image}
             download
-            className='bg-zinc-900 px-10 py-3 rounded-full curser-pointer'
+            className='bg-zinc-900 px-10 py-3 rounded-full cursor-pointer text-white'
           >
             Download
           </a>
