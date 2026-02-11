bd2bd35b18c0d0cd51e607e16717ab71db1719ed
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-11 16:13:39 +0530

    add login and sighup navbar

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+
 const Login = () => {
+  const [state, setState] = useState("Login");
+  const {setShowLogin} = useContext(AppContext)
+
+  useEffect(()=> {
+    document.body.style.overflow = 'hidden';
+
+    return ()=>{
+      document.body.style.overflow = 'unset'
+    }
+
+  },[])
+
   return (
     <div className='absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-      <form className='relative bg-white p-10 rounded-xl text-start-500'>
-        <h1 className='text-center text-2xl text-neutral-700 font-medium'>
-          Sign Up
-        </h1>
-        <p className='text-sm'>Welcome back! Please sign in to continue</p>
+      <form className='relative bg-white p-10 rounded-xl text-neutral-700'>
+        <h1 className='text-center text-2xl font-medium'>{state}</h1>
 
-        <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
-          <img src={assets.user_icon} alt='' />
-          <input
-            type='text'
-            className='outline-none text-sm'
-            placeholder='Full Name'
-            required
-          />
-        </div>
+        <p className='text-sm text-center'>
+          Welcome back! Please sign in to continue
+        </p>
 
+        {/* Full Name only in Sign up */}
+        {state !== "Login" && (
+          <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-5'>
+            <img src={assets.user_icon} alt='' />
+            <input
+              type='text'
+              className='outline-none text-sm'
+              placeholder='Full Name'
+              required
+            />
+          </div>
+        )}
+
+        {/* Email */}
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
           <img src={assets.email_icon} alt='' />
           <input
@@ -29,6 +48,7 @@ const Login = () => {
           />
         </div>
 
+        {/* Password */}
         <div className='border px-6 py-2 flex items-center gap-2 rounded-full mt-4'>
           <img src={assets.lock_icon} alt='' />
           <input
@@ -39,9 +59,47 @@ const Login = () => {
           />
         </div>
 
-        <p className="text-sm text-blue-600 my-4 cursor-pointer">Forget Password</p>
+        {/* Forget password */}
+        {state === "Login" && (
+          <p className='text-sm text-blue-600 my-4 cursor-pointer'>
+            Forget Password
+          </p>
+        )}
 
-        <button className="bg-blue-600 w-full text-white py-2 rounded-full">create account</button>
+        {/* Button */}
+        <button className='bg-blue-600 w-full text-white py-2 rounded-full'>
+          {state === "Login" ? "Login" : "Create Account"}
+        </button>
+
+        {/* Toggle Login / Signup */}
+        {state === "Login" ? (
+          <p className='mt-5 text-center'>
+            Don't have an account?{" "}
+            <span
+              className='text-blue-600 cursor-pointer'
+              onClick={() => setState("Sign up")}
+            >
+              Sign up
+            </span>
+          </p>
+        ) : (
+          <p className='mt-5 text-center'>
+            Already have an account?{" "}
+            <span
+              className='text-blue-600 cursor-pointer'
+              onClick={() => setState("Login")}
+            >
+              Login
+            </span>
+          </p>
+        )}
+
+        
+        <img onClick={()=>setShowLogin(false)}
+          src={assets.cross_icon}
+          alt=''
+          className='absolute top-5 right-5 cursor-pointer'
+        />
       </form>
     </div>
   );
