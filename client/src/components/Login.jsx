a39ec2f692c64e29f2d22cc03856806e103976cf
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-13 23:35:31 +0530

    add some changes in login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 
 import { assets } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
+import { motion } from "framer-motion";
 
 const Login = () => {
   const [state, setState] = useState("Login");
@@ -17,7 +18,13 @@ const Login = () => {
 
   return (
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
-      <form className='relative bg-white p-10 rounded-xl text-neutral-700'>
+      <motion.form
+        initial={{ opacity: 0.2, y: 50 }}
+        transition={{ duration: 0.5 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        className='relative bg-white p-10 rounded-xl text-neutral-700'
+      >
         <h1 className='text-center text-2xl font-medium'>{state}</h1>
 
         <p className='text-sm text-center'>
@@ -100,7 +107,7 @@ const Login = () => {
           alt=''
           className='absolute top-5 right-5 cursor-pointer'
         />
-      </form>
+      </motion.form>
     </div>
   );
 };
--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { assets, plans } from "../assets/assets";
 import { AppContext } from "../context/AppContext";
-import { motion } from "frame-motion";
+import { motion } from "framer-motion";
 
 const BuyCredit = () => {
   const { user } = useContext(AppContext);
