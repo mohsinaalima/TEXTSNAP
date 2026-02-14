6483a2b3729e1d087ee06d6824e95350730ecd4e
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-14 16:44:45 +0530

    add usercontroller

---
+++ server/controllers/userController.js
@@ -0,0 +1,23 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json(400).json({ success: false, message: "Missing Details" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+    const user = new userModel(userData);
+  } catch (error) {}
+};
