ebe3400364e68967859bf6ced6709fb6a5a02da6
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-15 00:15:09 +0530

    add some changes in appContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,12 +6,18 @@ const AppContextProvider = (props) => {
   const [user, setUser] = useState(null);
 
   const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
+  const [credit, setCredit] = useState(false)
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL
 
   const value = {
     user,
     setUser,
     showLogin,
     setShowLogin,
+    backendUrl
   };
 
   return (
