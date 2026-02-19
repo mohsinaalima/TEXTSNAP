af785ddf54a18fc2ff621055f733ebb5ec39d229
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-19 13:47:38 +0530

    add changes in userrouter.jsx

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { registerUser, loginUser, userCredits } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  userCredits,
+} from "../controllers/userController.js";
 import userAuth from "../middlewares/auth.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.post("/credits", userAuth , userCredits);
+userRouter.get("/credits", userAuth, userCredits);
 
 export default userRouter;
