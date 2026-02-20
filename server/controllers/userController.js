af5278168887beb2e759c23cb1ef47bb81080245
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-20 16:41:47 +0530

    fix:  detail in usercontrollers

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -173,6 +173,25 @@ const paymentRazorpay = async (req, res) => {
 
     const newTransaction = await transactionModel.create(transtionData);
 
+const option = {
+
+  amount: amount * 100,
+  currency: process.env.CURRENCY,
+  receipt: newTransaction._id,
+}
+
+    await razorpayInstance.orders.create(option, (error, order) => {
+      if (error) {
+        console.log(error);
+        return res.json({success: false, message: error})
+      }
+      res.json({
+        success: true,
+        order,})
+
+
+    })
+
 
   } catch (error) {
     console.log(error);
