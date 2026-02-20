7705dd6598da514933eb6b6eb514449243da3d61
Mohsina Alima <mohsinaalima2006@gmail.com>
2026-02-20 23:01:20 +0530

    add more changes in BuyCredit.jsx

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -22,7 +22,20 @@ const BuyCredit = () => {
       order_id: order.id,
       receipt: order.receipt,
       handler: async (response)=>{
-        console.log(response);
+       try {
+
+        const {data} = await axios.post(backendUrl + '/api/user/verify-razor', response, {headers: {token}})
+        if (data.success) {
+          loadCreditsData();
+          navigate('/')
+          toast.success('Credit Added')
+        }
+        
+
+
+       } catch (error) {
+        toast.error(error.message)
+       }
       }
     }
     const rzp = new window.Razorpay(options)
